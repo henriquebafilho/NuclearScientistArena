800756f92848effc4bc893d8347081e33381f088
Evelyn Correa <evelynnave2015@gmail.com>
2017-08-31 15:31:51 -0300

    ImplementsNow

---
+++ Assets/Resources/Scripts/GameMananger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameMananger : MonoBehaviour {
+	GameObject[] drains;
+	public GameObject particule, history;
+    public Text scoreText1;
+    public Text scoreText2;
+    public static int score1, score2;
+    public static int touchable = 0;
+	void Start () {
+        score1 = 0;
+        score2 = 0;
+		drains = GameObject.FindGameObjectsWithTag ("drain");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateScore();
+        Spawns();
+	}
+   
+	public void spawn()
+	{
+		StartCoroutine (Spawns ());
+	}
+	public IEnumerator  Spawns()
+	{
+		yield return new WaitForSeconds (4);
+		Instantiate (particule, drains [Random.Range (0, drains.Length)].transform.position, Quaternion.identity);
+	}
+    public void AddScore(int newScoreValue)
+    {
+        score1 += newScoreValue;
+        score2 += newScoreValue;
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        scoreText1.text = "Pontos: " + score1;
+        scoreText2.text = "Pontos: " + score2;
+        if (score1 == 50 || score2 == 50) { SceneManager.LoadScene("victory"); }
+    }
+}
---
+++ Assets/Resources/Scripts/buttons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour {
+
+    public GameObject sound;
+
+    void Update()
+    {
+        DontDestroyOnLoad(sound);
+    }
+
+    public void OnClickPlay(){
+		SceneManager.LoadScene ("Game1_1");
+	}
+	public void OnClickCredits(){
+		SceneManager.LoadScene ("Credits");
+	}
+	public void OnClickMenu(){
+		SceneManager.LoadScene ("Menu");
+	}
+    public void OnClickHistory() {
+        SceneManager.LoadScene("History");
+    }
+	public void OnClickExit(){
+		Application.Quit ();
+	}
+    public void goToscene()
+    {
+        SceneManager.LoadScene("Game1_1");
+    }
+    public void OnClickTips() {
+        SceneManager.LoadScene("Tips");
+
+    }
+}
